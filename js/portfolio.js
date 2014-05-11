331c2d4d6b9532a8c5c4d0d28d4debb1fcff0aaf
Charlie <annecharlotte.gerard@gmail.com>
2014-05-11 13:27:59 +1000

    slide projects fixed

--- js/portfolio.js
+++ js/portfolio.js
@@ -59,7 +59,82 @@ $(document).ready(function(){
 		$('#readr #bloc').slideUp(200);
 	});
 
+
+	$('#dancingcats').hover(function(){
+		$('#dancingcats #bloc').slideDown(200);
+	}, function(){
+		$('#dancingcats #bloc').slideUp(200);
+	});
+
+
+	//Progress bar
+
+		var maxWidth = 400;
+	    var duration = 2000;
+	    var timer;
+
+	$(window).load(function(){
+
+	        var $bar = $('#bar').width(0).css('background-color', 'rgb(12, 21, 32)');
+	        var $bar2 = $('#bar2').width(0).css('background-color', 'rgb(52,103,124)');
+	        var $bar3 = $('#bar3').width(0).css('background-color', 'rgb(98,151,145)');
+	        var $bar4 = $('#bar4').width(0).css('background-color', 'rgb(137,179,148)');
+	       	var $bar5 = $('#bar5').width(0).css('background-color', 'rgb(196,0,0)');
+	       	var $bar6 = $('#bar6').width(0).css('background-color', 'grey');
+	        //Displaying a percentage on the screen.
+	        //Horloge(maxWidth);
+	        //timer = setInterval('Horloge(' + maxWidth + ')', 100);
+	        $bar.animate({
+	            width: 200 + 'px'
+	        }, duration, function () {
+	            $(this).css('background-color', 'rgb(12, 21, 32)');
+	            //clearInterval(timer);
+	        });
+
+	       	$bar2.animate({
+	            width: 180 + 'px'
+	        }, duration, function () {
+	            $(this).css('background-color', 'rgb(52,103,124)');
+	            //clearInterval(timer);
+	        });
+
+	        $bar3.animate({
+	            width: 160 + 'px'
+	        }, duration, function () {
+	            $(this).css('background-color', 'rgb(98,151,145)');
+	            //clearInterval(timer);
+	        });
+
+	       	$bar4.animate({
+	            width: 50 + 'px'
+	        }, duration, function () {
+	            $(this).css('background-color', 'rgb(137,179,148)');
+	            //clearInterval(timer);
+	        });
+
+	       	$bar5.animate({
+	            width: 100 + 'px'
+	        }, duration, function () {
+	            $(this).css('background-color', 'rgb(196,0,0)');
+	            //clearInterval(timer);
+	        });
+
+	        $bar6.animate({
+	            width: 100 + 'px'
+	        }, duration, function () {
+	            $(this).css('background-color', 'grey');
+	            //clearInterval(timer);
+	        });
+	});
+
 });
+
+
+	function Horloge(maxWidth) {
+	    var w = $('#bar').width();
+	    var percent = parseInt((w * 100) / maxWidth);
+	    $('#log').html(percent + ' %');
+	}
 
 
 
