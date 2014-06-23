318aaaeb4a72762aacb23437af0b15c6f709f56e
Charlie <annecharlotte.gerard@gmail.com>
2014-06-23 15:36:34 +1000

    fixfix responsive design for GA projects

--- js/portfolio.js
+++ js/portfolio.js
@@ -36,72 +36,45 @@ $(document).ready(function(){
 	TEST WITH CAROUSEL PROJECTS
 	*/
 
-	$('#project_tiles').slick({
-		dots: true
-	})
-
-	/*----------------
-	Trying to create a kind of javascript shell - Not used at the moment
-	-----------------*/
-
-	// var shellIn=$('#shellIn');
-	// var shellOut=$("#shellOut");
-	// var shellDiv=$("#shellDiv");
-	// shellIn.focus();
-
-	// function shellEvaluate() {
-	// 	shellOut.innerHTML+="<b>"+shellIn.value+"</b><br/><i>"+ eval(shellIn.value)+"</i><br/>>>> "
-	// 	//shellDiv.scrollTop=shellDiv.scrollHeight
-	// 	//shellIn.value="";
-	// 	shellIn.value = $('#shellIn').val()
-
-	// 	if(shellIn.value == "1"){
-	// 	 	// shellOut.innerHTML += "Hello back"
-	// 	 	$('#answer').append(">>> If I could travel in time, I'd like to go check out the dinosaurs... Dinosaurs are awesome!" + "<br><br>")
-	// 	} else if (shellIn.value == '2'){
-	// 		//$('#answer').empty();
-	// 		$('#answer').append(">>> If I could choose to be someone famous, I'd be Grace Hopper, she was a badass..." + "<br><br>")
-	// 	} else if (shellIn.value == '3'){
-	// 		$('#answer').append(">>> I can make animals with balloons..." + "<br><br>")
-	// 	}
-	// };
-
-	// $('#shellIn').on('keydown', function(){
-	// 	if(event.keyCode==13){
-	// 		shellEvaluate();
-	// 	}
-	// });
+	//Display the GA projects in carousel if not on mobile
+	if(window.innerWidth > 481){
+		$('#project_tiles').slick({
+			dots: true
+		})
+	
 
 	/*-------------------------------------------------------- 
 				PROJECTS SLIDES WITH INFOS ON HOVER
 	---------------------------------------------------------*/
 
-	$('#readr').mouseenter(function(){
-		$('#readr_content').slideToggle(150);
-		$('#readr h3').animate({bottom: '100px'}, 150);
-		// return false;
-	}).mouseleave(function(){
-		$('#readr_content').slideToggle(150);
-		$('#readr h3').animate({bottom: '0px'}, 150);
-	});
+		$('#readr').mouseenter(function(){
+			$('#readr_content').slideToggle(150);
+			$('#readr h3').animate({bottom: '100px'}, 150);
+			// return false;
+		}).mouseleave(function(){
+			$('#readr_content').slideToggle(150);
+			$('#readr h3').animate({bottom: '0px'}, 150);
+		});
 
-	$('#loople').mouseenter(function(){
-		$('#loople_content').slideToggle(150);
-		$('#loople h3').animate({bottom: '100px'}, 150);
-		// return false;
-	}).mouseleave(function(){
-		$('#loople_content').slideToggle(150);
-		$('#loople h3').animate({bottom: '0px'}, 150);
-	});
+		$('#loople').mouseenter(function(){
+			$('#loople_content').slideToggle(150);
+			$('#loople h3').animate({bottom: '100px'}, 150);
+			// return false;
+		}).mouseleave(function(){
+			$('#loople_content').slideToggle(150);
+			$('#loople h3').animate({bottom: '0px'}, 150);
+		});
 
-	$('#spectrm').mouseenter(function(){
-		$('#spectrm_content').slideToggle(150);
-		$('#spectrm h3').animate({bottom: '100px'}, 150);
-		// return false;
-	}).mouseleave(function(){
-		$('#spectrm_content').slideToggle(150);
-		$('#spectrm h3').animate({bottom: '0px'}, 150);
-	});
+		$('#spectrm').mouseenter(function(){
+			$('#spectrm_content').slideToggle(150);
+			$('#spectrm h3').animate({bottom: '100px'}, 150);
+			// return false;
+		}).mouseleave(function(){
+			$('#spectrm_content').slideToggle(150);
+			$('#spectrm h3').animate({bottom: '0px'}, 150);
+		});
+
+	}
 
 	/* ---------------------------------------------
 				SKILLS
