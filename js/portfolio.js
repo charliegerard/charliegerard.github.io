a8cb3534b138a087a8987ac3bb6afccc7b4dfd2a
Charlie gerard <annecharlotte.gerard@gmail.com>
2016-01-24 15:38:47 +1100

    removing processing folder and adding codepen sketches

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,7 @@
 $(document).ready(function(){
-
-	/*When clicking on one of the link in the navbar, scroll down automatically
-	to that part */
 	$('#navbar a').on('click', function(event){
 		var $anchor = $(this);
-		console.log($anchor.attr('href').height)
 		$('html, body').animate({
-			//scrollTop: $anchor.attr('href').offsetTop + 'px'
 			scrollTop: $($anchor.attr('href')).offset().top + 'px'
 		}, 1000);
 		event.preventDefault();
@@ -14,19 +9,13 @@ $(document).ready(function(){
 
 	$('#about a').on('click', function(event){
 		var $anchor = $(this);
-		console.log($anchor.attr('href').height)
 		$('html, body').animate({
-			//scrollTop: $anchor.attr('href').offsetTop + 'px'
 			scrollTop: $($anchor.attr('href')).offset().top + 'px'
 		}, 1000);
 		event.preventDefault();
 	});
 
-	/*
-	LIST OF THINGS I LIKE. RENDERING ONE WORD AT A TIME
-	*/
-
-	var words = ["Programming", "Post-rock", "LEGO", "Fractals",
+	var words = ["Programming", "Post-rock", "LEGO",
 				"Robots", "Creative Innovations", "Coffee",
 				"Generative Art", "Learning"]
 	var counter = 0;
@@ -43,9 +32,3 @@ $(document).ready(function(){
 	window.setInterval(change, 1500);
 
 });
-
-
-
-
-
-
